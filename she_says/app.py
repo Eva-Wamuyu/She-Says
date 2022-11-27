e8aafb57764a03cab02c441126e50161f9f1b8a3
Eva-Wamuyu <rutheve.eva@gmail.com>
2022-11-27 17:11:40 +0300

    add middleware to allow all origins

--- she_says/app.py
+++ she_says/app.py
@@ -1,14 +1,33 @@
 from fastapi import  FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import  models,router
 from .db import  engine
 
-models.Base.metadata.create_all(bind=engine)
-
-
 
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = "*",
+    allow_credentials = True,
+    allow_methods=["*"],
+    allow_headers = ["*"]
+)
+
+
+@app.get("/")
+async def index():
+    return {
+        "message": "Go to /docs for more information"
+    }
+
+models.Base.metadata.create_all(bind=engine)
 app.include_router(router.author)
 app.include_router(router.book)
